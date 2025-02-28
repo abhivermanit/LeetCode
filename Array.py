110016bc8b09cd5059d7243288c759275780ef71
ABHISHEK VERMA <erabhimech7@gmail.com>
2025-02-28 23:23:58 +0530

    Create Array.py

---
+++ Array.py
@@ -0,0 +1,18 @@
+# 268. Missing Number
+
+My Approach
+
+class Solution(object):
+    def missingNumber(self, nums):
+        self.nums = nums # See this is not needed as there is only one function, had there been multiple we would need this instance variable initialization
+        nums.sort()
+        for i in range(0,len(nums)+1):
+            if i in nums:
+                continue
+            else:
+                return i
+
+  Let's optimize this solution :-
+
+ 
+
