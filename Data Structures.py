9978d7ee009b8ed72951c41a598121ed51b53e07
ABHISHEK VERMA <erabhimech7@gmail.com>
2025-03-06 18:17:33 +0530

    Create Data Structures.py

---
+++ Data Structures.py
@@ -0,0 +1,22 @@
+# Linked List 
+
+class Node:  # making a node
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def insertAtBegin(self, data):
+    new_node = Node(data)
+    if self.head is None:
+        self.head = new_node
+        return
+    else:
+        new_node.next = self.head
+        self.head = new_node
+
+
+
+
+
+
